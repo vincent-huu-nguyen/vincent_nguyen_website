c543309761fd669c8dbe64085e3edccb66f373e7
vinsanite <vincentnguyen7011@gmail.com>
2025-06-20 14:56:28 -0500

    updated portfolio page

--- src/routes/Portfolio.jsx
+++ src/routes/Portfolio.jsx
@@ -14,10 +14,22 @@ import vgdoWebsite from "../assets/VGDOwebsite.png"
 import redPandaBash from "../assets/redPandaBash.png"
 import battleRPB from "../assets/battleRPB.gif"
 import battleRPB2 from "../assets/battleRPB2.gif"
+import musicChanges from "../assets/musicChanges.png"
+import musicJourney from "../assets/musicJourney.png"
+import musicProm from "../assets/musicProm.png"
 
 
 const Portfolio = () => {
     const portfolio = useMemo(() => [
+        {
+            img: musicChanges,
+            title: "Cents Music Player",
+            desc: " A fully functional web-based music player featuring a curated collection of original tracks produced by me. Stream and explore my music directly from your browser.",
+            live: "https://vincent-huu-nguyen.github.io/Cents_Music_Player/",
+            git: "https://github.com/vincent-huu-nguyen/Cents_Music_Player",
+            hoverClass: "hover:bg-gradient-to-t from-[#7382ad] via-[#f7f7f9] to-[#f9f9f9]",
+            slideshowImages: [musicJourney, musicProm, musicChanges],
+        },
         {
             img: redPandaBash,
             title: "Red Panda Bash",
@@ -28,15 +40,6 @@ const Portfolio = () => {
             slideshowImages: [battleRPB, battleRPB2],
         },
         {
-            img: vgdoWebsite,
-            title: "VGDO Website",
-            desc: " The official website for the VGDO (Video Game Developers Organization) at UTA. It is a central hub for club updates, event details, jam submissions, and resources for students interested in game development.",
-            live: "https://uta-vgdo.github.io/website/",
-            git: "https://github.com/uta-vgdo/website",
-            hoverClass: "hover:bg-gradient-to-t from-[#7382ad] via-[#f7f7f9] to-[#f9f9f9]",
-            slideshowImages: [vgdoWebsite],
-        },
-        {
             img: PortfolioWebsite,
             title: "Portfolio Website",
             desc: " My personal portfolio website showcasing projects in software engineering, web and game development.",
@@ -44,6 +47,15 @@ const Portfolio = () => {
             git: "https://github.com/vincent-huu-nguyen/vincent_nguyen_website",
             hoverClass: "hover:bg-gradient-to-t from-[#7382ad] via-[#f7f7f9] to-[#f9f9f9]",
             slideshowImages: [PortfolioWebsite],
+        },
+        {
+            img: vgdoWebsite,
+            title: "VGDO Website",
+            desc: " The official website for the VGDO (Video Game Developers Organization) at UTA. It is a central hub for club updates, event details, jam submissions, and resources for students interested in game development.",
+            live: "https://uta-vgdo.github.io/website/",
+            git: "https://github.com/uta-vgdo/website",
+            hoverClass: "hover:bg-gradient-to-t from-[#7382ad] via-[#f7f7f9] to-[#f9f9f9]",
+            slideshowImages: [vgdoWebsite],
         },
         {
             img: rhythmixLogo,
@@ -84,7 +96,7 @@ const Portfolio = () => {
     return (
         <>
             <NavBar />
-            <section className="bg-primary px-5 text-[#181818] pb-10">
+            <section className="bg-primary px-5 text-[#181818] pb-56">
                 <div className="container mx-auto">
                     <div className="relative flex justify-center items-center m-10 flex-wrap text-left">
                         <h1 className="text-4xl font-bold mb-4 hover:bg-gradient-to-r from-green-500 via-indigo-500 to-purple-500 hover:text-transparent hover:bg-clip-text hover:scale-110 duration-200">
@@ -104,7 +116,7 @@ const Portfolio = () => {
                                     setCurrentImageIndex(0);
                                 }}
                             >
-                                <div className={`bg-[#F3F4F6] ${item.hoverClass} p-4 rounded-md shadow-md`}>
+                                <div className={`bg-[#F3F4F6] ${item.hoverClass} p-4 rounded-md shadow-md h-[460px] flex flex-col justify-between`}>
                                     <img
                                         src={hoveredIndex === index ? item.slideshowImages[currentImageIndex] : item.img}
                                         alt={item.title}
@@ -113,12 +125,14 @@ const Portfolio = () => {
 
                                     <p className="text-lg font-semibold">{item.title}</p>
                                     <p className="text-sm text-gray-600">{item.desc}</p>
-                                    <a
-                                        href={item.git}
-                                        target="_blank"
-                                        className="bg-[#f0f0f0] p-2 font-semibold text-white inline-flex items-center space-x-2 rounded-full hover:scale-110 duration-200">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>
-                                    </a>
+                                    <div className="mt-4 flex justify-center">
+                                        <a
+                                            href={item.git}
+                                            target="_blank"
+                                            className="w-8 h-8 bg-[#f0f0f0] text-white font-semibold rounded-full flex items-center justify-center hover:scale-110 duration-200">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>
+                                        </a>
+                                    </div>
                                 </div>
                             </a>
                         ))}
